4bead1a3ccfc1714c1c494f8720c764e4f3b182f
Jan Hamal Dvořák <mordae@anilinux.org>
2018-01-10 13:23:31 +0100

    Fix info.rkt
    Signed-off-by: Jan Hamal Dvořák <mordae@anilinux.org>

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang setup/infotab
 
 (define collection 'multi)
-(define deps '("base" "scribble-lib"))
-(define build-deps '("racket-doc"))
+(define deps '("base"))
+(define build-deps '("racket-doc" "scribble-lib"))
 
 ; vim:set ts=2 sw=2 et:
